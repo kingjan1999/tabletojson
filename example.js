86d8e1d4baff718b0ece0609ff50e4e052fcf03b
kingjan1999 <king-jan1999@hotmail.de>
2017-03-18 17:49:24 +0100

    rename package again

--- example.js
+++ example.js
@@ -1,4 +1,4 @@
-var tabletojson = require('table-to-json');
+var tabletojson = require('tableasjson');
 tabletojson.convertUrl('https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes', function(tablesAsJson) {
   // Print ou the first table on the above webpage as a JSON object
   console.log(tablesAsJson[1]);
